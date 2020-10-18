b0a520a146e1f0470822d337fad74711910d24ff
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-18 17:34:49 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,11 +5,9 @@ module ArtistsHelper
 
   def artist_select(song, path)
     if song.artist && path == "nested"
-      select_tag "song[artist_id]"
       hidden_field_tag "song[artist_id]", song.artist_id
     else
-      options_from_collection_for_select(Artist.all, :id, :name)
-      hidden_field_tag "song[artist_id]", song.artist_id
+      select_tag "song[artist_id]" options_from_collection_for_select(Artist.all, :id, :name)
     end
   end
 end
